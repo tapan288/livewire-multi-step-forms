f4cb935ebc6d99e482b2f2318d01b522dc51ff10
Tapan Sharma <tapansharma69@gmail.com>
2024-02-10 22:06:13 +0545

    navigating steps

--- app/Livewire/Wizard/Create/Steps/TitleStep.php
+++ app/Livewire/Wizard/Create/Steps/TitleStep.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Wizard\Create\Steps;
 
+use Livewire\Attributes\Validate;
 use Spatie\LivewireWizard\Components\StepComponent;
 
 class TitleStep extends StepComponent
 {
+    #[Validate('required')]
+    public $title;
+
+    public function submit()
+    {
+        $this->validate();
+
+        $this->nextStep();
+    }
+
     public function render()
     {
         return view('livewire.wizard.create.steps.title-step');
--- resources/views/livewire/wizard/create/steps/description-step.blade.php
+++ resources/views/livewire/wizard/create/steps/description-step.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <form class="space-y-6">
+        <div>
+            <x-input-label for="description" :value="__('Description')" />
+            <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" />
+            <x-input-error :messages="$errors->get('description')" class="mt-2" />
+        </div>
+
+        <x-primary-button>
+            {{ __('Next step') }}
+        </x-primary-button>
+    </form>
 </div>
