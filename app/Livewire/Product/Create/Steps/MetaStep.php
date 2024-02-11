9eec773cd7b273d8e14315d3085c5ca8556fdf2c
Tapan Sharma <tapansharma69@gmail.com>
2024-02-11 23:14:26 +0545

    creating the product up front

--- app/Livewire/Product/Create/Steps/MetaStep.php
+++ app/Livewire/Product/Create/Steps/MetaStep.php
@@ -2,15 +2,34 @@
 
 namespace App\Livewire\Product\Create\Steps;
 
+use Livewire\Attributes\Validate;
 use Spatie\LivewireWizard\Components\StepComponent;
 
 class MetaStep extends StepComponent
 {
+    #[Validate('required')]
+    public string $title = '';
+
+    #[Validate('required')]
+    public $description = '';
+
     public function stepInfo(): array
     {
         return [
             'label' => 'Meta Information',
         ];
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        auth()->user()->products()->create([
+            'title' => $this->title,
+            'description' => $this->description,
+        ]);
+
+        $this->nextStep();
     }
 
     public function render()
